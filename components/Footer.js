8a523ad4e583de532448d23117f48a6b5403ce9b
Jake Tyler Simon <jtsimonbaseball@gmail.com>
2022-03-03 00:04:52 -0800

    fooer update

--- components/Footer.js
+++ components/Footer.js
@@ -5,11 +5,11 @@ const Footer = () => {
   return (
     <footer className='grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500'>
         
-        <div className='px-8 py-3'>
+        {/* <div className='px-8 py-3'>
             <p>
                 United States
             </p>
-        </div>
+        </div> */}
 
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 px-8 py-3 grid-flow-row-dense'>
             <div className='flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2 '>
